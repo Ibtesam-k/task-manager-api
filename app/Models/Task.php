cd701147ada47c1b8255caa65d6f4bc7aaa134d0
Ibtesam.k <ibtesam.kadourah@gmail.com>
2026-08-28 14:31:59 +0300

    Add task model and migration

---
+++ app/Models/Task.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TaskStatus;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Task extends Model
+{
+    use HasUuids, SoftDeletes;
+    protected $fillable = ['project_id','created_by','assignee_id','title','description','start_date','due_date','status'];
+
+    public function uniqueIds() : array
+    {
+        return ['uuid'];
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return 'uuid';
+    }
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function assignee(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'assignee_id');
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'status' => TaskStatus::class,
+            'start_date' => 'date',
+            'due_date' => 'date',
+        ];
+    }
+
+
+}
---
+++ database/migrations/2026_08_28_105746_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->foreignId('project_id')->constrained();
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('assignee_id')->nullable()->constrained('users');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('status')->default('todo');
+            $table->date('start_date')->nullable();
+            $table->date('due_date')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
